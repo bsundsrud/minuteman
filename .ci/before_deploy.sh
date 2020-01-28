21bbe21b5d9e9584a4ab79bbb39db82bd1ead67b
Benn Sundsrud <benn.sundsrud@gmail.com>
2020-01-28 15:30:40 +0100

    builds, amirite?

--- .ci/before_deploy.sh
+++ .ci/before_deploy.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-is_linux() {
-    case "$TRAVIS_OS_NAME" in
-        linux) return 0 ;;
-        *)   return 1 ;;
-    esac
-}
 
-if is_linux; then
-    rustup target add x86_64-unknown-linux-musl
-fi
-
-make init build-prod dist VERSION="$TRAVIS_TAG" TARGET="$TARGET"
+make build-prod dist VERSION="$TRAVIS_TAG" TARGET="$TARGET"
